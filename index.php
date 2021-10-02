08b1565d6fc9d8a020ba90c207a119b0bde6aa14
artpros94 <prostakishyn.artem@icloud.com>
2021-10-02 10:52:10 +0300

    family

--- index.php
+++ index.php
@@ -8,70 +8,49 @@ $db_name = 'sql';
 $link = mysqli_connect($host, $user, $pass, $db_name);
 mysqli_query($link, "SET NAMES 'utr8'");
 
+
 /****************************
-    Insert 10 users 
+    Insert 20 users end Son ID
 ****************************/  
-// for($i = 1; $i <= 10; $i++){
 
-//     $value = 'user ' . $i;
-
-//     $query = "INSERT INTO users (name) VALUE ('$value')";
-//     mysqli_query($link, $query) or die(mysqli_error($link));
-// }
-/****************************
-    Insert 20 cities 
-****************************/ 
 // for($i = 1; $i <= 20; $i++){
 
-//     $value = 'city ' . $i;
+//     $user = 'User ' . $i;
 
-//     $query = "INSERT INTO cities (name) VALUE ('$value')";
+//     if($i == 1) {
+//         $sonId = 1;
+//     } else {
+//         $sonId = rand(1, $i - 1);
+//     }   
+
+//     $query = "INSERT INTO family_tree (user, son_id) VALUE ('$user', '$sonId')";
+
 //     mysqli_query($link, $query) or die(mysqli_error($link));
+    
 // }
 
-/****************************
-    Insert 30 connections user whis cities
-****************************/ 
-// for($i = 1; $i <= 30; $i++){
 
-//     $cityId = rand(1, 20);
-//     $userId = rand(1, 10);
 
-//     $query = "INSERT INTO connection (user_id, city_id) VALUE ('$userId', '$cityId')";
-//     mysqli_query($link, $query) or die(mysqli_error($link));
+$query = "SELECT family_tree.user as son, sons.user as father
+            FROM family_tree
+            LEFT JOIN family_tree as sons ON sons.id=family_tree.son_id";
 
-// }  
 
-// ****************************************************
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+for($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
 
-    $query = "SELECT 
-            users.name as user_name,
-            cities.name as city_name
-            FROM users 
-            LEFT JOIN connection ON connection.user_id=users.id
-            LEFT JOIN cities ON connection.city_id=cities.id";
 
-    $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
-    for($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-
-    sort($data);
-
-    $res = [];
-    foreach($data as $elem){
-        $res[$elem['user_name']][] = $elem['city_name'];
+foreach($data as $fatherSon){
+    if($fatherSon['father'] == null) {
+        echo 'Forefather is ' .  $fatherSon['son'] .'<br>';
+    } else {
+        echo 'Father ' . $fatherSon['father'] . ' end they son is ' . $fatherSon['son']. '<br>';
     }
 
-    
-    foreach($res as $value => $key){
-        
-        echo $value;
-        echo "<ul>";
-            if(is_array($key)){
-                foreach($key as $value){
-                    echo "<li>{$value}</li>";
-                }
-            }
-        echo "</ul><br>";
+}
 
-    }
+
+
+
+
